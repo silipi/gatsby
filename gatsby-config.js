4308cbac7c8d6e9320a9530b69d992143d045a8f
silipi <gabriel.silipi@gmail.com>
2020-03-19 13:21:22 -0300

    Limpando as coisas do Starter

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Meu blog`,
+    description: `Um blog sobre um desenvolvedor front-end e outras coisas legais.`,
+    author: `@myblog`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
